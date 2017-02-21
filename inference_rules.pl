def9a0a1b1f836e8c2bdce6960d4f9b20ca6393c
Kevin McAreavey <kevin.mcareavey@gmail.com>
2017-02-21 18:00:30 +0000

    Added second inference rule
    Added second inference rule for stand events, and updated results.

--- inference_rules.pl
+++ inference_rules.pl
@@ -2,9 +2,15 @@
 % Inference rules.
 %
 
-0.9::infer([happens(e(S), T), subject(e(S), S), act(e(S), sit)]) :-
+0.9::infer([happens(e(S, sit), T), subject(e(S, sit), S), act(e(S, sit), sit)]) :-
 	happens(E, T),
 	subject(E, S),
 	orientation(E, sitting),
 	holdsAt(orientation(S, standing), T).
+
+0.9::infer([happens(e(S, stand), T), subject(e(S, stand), S), act(e(S, stand), stand)]) :-
+    happens(E, T),
+    subject(E, S),
+    orientation(E, standing),
+    holdsAt(orientation(S, sitting), T).
 
--- scripts/format-inference_queries.sh
+++ scripts/format-inference_queries.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 results="../results/inference_queries"
 mkdir -p "$results"
-echo -e "\tinfer([happens(e(S),T),subject(e(S),S),act(e(S),sit)])"
-cat "$results/kb.log" | tr -d '[:blank:]' | sed 's/:/\t/g' | sed 's/infer(\[happens(e(/{S\//g' | sed 's/),/,T\//g' | sed 's/,T\/subject/}\t/g' | cut -f1,3
+echo -e "\tinfer([happens(e(S,A),T),subject(e(S,A),S),act(e(S,A),A)])"
+cat "$results/kb.log" | tr -d '[:blank:]' | sed 's/:/\t/g' | sed 's/infer(\[happens(e(/{S\//g' | sed 's/),/,T\//g' | sed 's/,T\/subject/}\t/g' | sed 's/sit/A\/sit/g' | sed 's/stand/A\/stand/g' | cut -f1,3
